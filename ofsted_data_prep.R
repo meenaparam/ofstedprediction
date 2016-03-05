ab508ecf00728460facc2477fe9a3ecc42f65242
Meenakshi Parameshwaran <meena.param@gmail.com>
2016-03-05 20:15:08 +0000

    Final data prep file - created schools df used in the app.

--- ofsted_data_prep.R
+++ ofsted_data_prep.R
@@ -34,6 +34,7 @@ names(myofsted) <- myofstednames
 # Check and fix column types in myofsted df
 lapply(myofsted, class)
 myofsted$instype <- factor(myofsted$instype)
+myofsted$urn <- as.integer(myofsted$urn)
 
 p_load(plyr) # for recoding
 myofsted$instype <- mapvalues(myofsted$instype, 
@@ -57,6 +58,62 @@ myofsted$instype <- mapvalues(myofsted$instype,
 
 myofsted$ofstedgrade <- factor(myofsted$ofstedgrade, levels = c(1,2,3,4), labels = c("Outstanding", "Good", "Requires Improvement", "Inadequate"))
 
-# Load in selected columns of the KS4 dataset
-ks4 <- openxlsx::read.xlsx(xlsxFile = "england_ks4.xlsx", sheet = 1, colNames = TRUE, startRow = 2, cols = c(3,4,5,16,17,18,19,27,96))
+# Load in selected columns of the KS4 dataset - this loading doesn't work
+# ks4 <- openxlsx::read.xlsx(xlsxFile = "england_ks4.xlsx", sheet = 1, colNames = TRUE, startRow = 2, cols = c(5,17,19,27,96))
 
+# Unfortunately, need to manually load the above file in Excel and then copy and paste the targe columns into a fresh csv file.
+
+# Load in the data using the pre-prepared csv file
+ks4 <- read.csv(file = "ks4.csv", stringsAsFactors = FALSE, blank.lines.skip = TRUE)
+
+# Tidy up the data - convert columns to factors
+ks4$RELDENOM <- as.factor(ks4$RELDENOM)
+ks4$EGENDER <- as.factor(ks4$EGENDER)
+
+# Tidy up the data - recode the factors above
+ks4$RELDENOM <- mapvalues(x = ks4$RELDENOM, 
+                          from = c(levels(ks4$RELDENOM)[1:5],
+                                   levels(ks4$RELDENOM)[6],
+                                   levels(ks4$RELDENOM)[7:22]
+                                   ),
+                          to = c(rep("Religious", 5),
+                                 "Non-religious",
+                                 rep("Religious", 16)
+                                )
+                        )
+
+ks4$EGENDER <- mapvalues(x = ks4$EGENDER,
+                         from = c(levels(ks4$EGENDER)[1],
+                                  levels(ks4$EGENDER)[2:3],
+                                  levels(ks4$EGENDER)[4]
+                                  ),
+                         to = c("Mixed",
+                                rep("Single-sex", 2),
+                                "Mixed"
+                                )
+                         )
+
+# Tidy up the data - fix latter two numeric columns
+ks4$KS2APS[ks4$KS2APS == "NP"] <- NA
+ks4$KS2APS[ks4$KS2APS == "SUPP"] <- NA
+ks4$KS2APS <- as.numeric(ks4$KS2APS)
+
+ks4$TTAPSCP_PTQ_EE[ks4$TTAPSCP_PTQ_EE == "NE"] <- NA
+ks4$TTAPSCP_PTQ_EE[ks4$TTAPSCP_PTQ_EE == "SUPP"] <- NA
+ks4$KS4APS <- as.numeric(ks4$TTAPSCP_PTQ_EE)
+ks4$TTAPSCP_PTQ_EE <- NULL
+
+# Merge the myosfted df to the ks4 df
+schools <- dplyr::inner_join(x = ks4, y = myofsted, by = c("URN" = "urn"))
+
+# Drop the laestab column
+schools$laestab <- NULL
+
+# Make all column names lowercase
+colnames(schools) <- lapply(colnames(schools), function(x) tolower(x))
+
+# Fix the region and totpups column classes
+schools$region <- as.factor(schools$region)
+schools$totpups <- as.integer(schools$totpups)
+
+# Data prep complete - use the schools dataset in the ofsted prediction app
